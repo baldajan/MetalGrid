9f43da08798335ceb793670c8e00d27b3a81283b
Basil Al-Dajane <basil@medlylabs.com>
2018-01-25 07:34:48 -0500

    Initial Commit

---
+++ MetalGrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalGrid
+//
+//  Created by Basil Al-Dajane on 2018-01-25.
+//  Copyright © 2018 Medly Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
